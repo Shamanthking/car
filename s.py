5713b7243b34e337034bf32c559a33958474afa1
Shamanthking <160223337+Shamanthking@users.noreply.github.com>
2024-10-27 13:09:04 +0530

    Update s.py

--- s.py
+++ s.py
@@ -3,7 +3,6 @@ import pandas as pd
 import numpy as np
 from sklearn.ensemble import RandomForestRegressor, GradientBoostingRegressor
 from sklearn.model_selection import train_test_split
-from sklearn.preprocessing import OneHotEncoder
 from sklearn.metrics import mean_squared_error, mean_absolute_error, r2_score
 import plotly.express as px
 import plotly.graph_objects as go
